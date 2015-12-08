633b599a1fb606f36ccdc4c540650645826071ee
Luís Serralheiro <encavadas@gmail.com>
2015-12-08 16:05:11 +0000

    Improved the array diff algorithm

--- jsonDiff.js
+++ jsonDiff.js
@@ -9,37 +9,48 @@
     var _isObject = function (value) {
       return !!value && typeof value === "object" && value !== null;
     };
-  
+
     var _isNotNull = function (value) {
       return value !== null && value !== undefined;
     };
-  
+
     var _isArray = function (value) {
-      return !!value && typeof value === "object" && value.constructor === Array;
+      return !!value && typeof value === "object" && Array.isArray(value);
     };
-  
+
     var _getChangesBetweenArrays = function (a, b, minimal) {
       var changes = {};
       a.forEach(function (elA, idx) {
-        var subChanges = _getChangesBetweenObjects(elA, b[idx], minimal);
-        if (!minimal || typeof subChanges !== 'object' || Object.keys(subChanges).length > 0) {
-          changes[idx] = subChanges;
+        if (!b[idx]) {
+          changes[idx] = 'removed';
+        } else {
+          var subChanges = _getChangesBetweenObjects(elA, b[idx], minimal);
+          if (!minimal || typeof subChanges !== 'object' || Object.keys(subChanges).length > 0) {
+            changes[idx] = subChanges;
+          }
         }
       });
+
+      b.forEach(function (val, idx) {
+        if (!a[idx]) {
+          changes[idx] = 'added';
+        }
+      });
+
       return changes;
     };
-  
+
     var _getChangesBetweenObjects = function (a, b, minimal) {
       var changes = {};
       var prop;
-  
+
       if (!_isObject(a) || !_isObject(b)) {
         if (a !== b) {
           changes = 'changed';
         }
         return changes;
       }
-  
+
       for (prop in b) {
         if (b.hasOwnProperty(prop)) {
           if (a.hasOwnProperty(prop)) {
@@ -63,30 +74,30 @@
           }
         }
       }
-  
+
       for (prop in a) {
         if (a.hasOwnProperty(prop) && !b.hasOwnProperty(prop)) {
           changes[prop] = 'removed';
         }
       }
-  
+
       return changes;
     };
-  
+
     var _getDiffInfo = function (a, b) {
       return _getChangesBetweenObjects(a, b, true);
     };
-  
+
     var _getMergeInfo = function (a, b) {
       var diff = _getChangesBetweenObjects(a, b);
       var result = {};
       var prop;
-  
+
       for (prop in a) {
         if (a.hasOwnProperty(prop)) {
           if (!_isObject(diff[prop])) {
             if (diff[prop]) {
-              result[prop] = { ___leftValue: a[prop], ___rightValue: b[prop], ___type: diff[prop] };
+              result[prop] = {___leftValue: a[prop], ___rightValue: b[prop], ___type: diff[prop]};
               result.___type = 'innerChange';
             }
             else {
@@ -103,18 +114,18 @@
           }
         }
       }
-  
+
       for (prop in diff) {
         if (diff.hasOwnProperty(prop) && !_isObject(diff[prop])) {
-          result[prop] = { ___leftValue: a[prop], ___rightValue: b[prop], ___type: diff[prop] };
+          result[prop] = {___leftValue: a[prop], ___rightValue: b[prop], ___type: diff[prop]};
           result.___type = 'innerChange';
         }
       }
-  
+
       return result;
     };
-  
-    return{
+
+    return {
       isObject: _isObject,
       isNotNull: _isNotNull,
       extendedDiff: _getChangesBetweenObjects,
